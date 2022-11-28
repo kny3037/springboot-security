e1187f82dffc3a535e4bb0742ff4c3b8812f8846
psi5059 <psi5059@naver.com>
2022-11-28 15:44:37 +0900

    @Builder 어노테이션 추가

--- src/main/java/com/hospital/review/domain/dto/UserJoinRequest.java
+++ src/main/java/com/hospital/review/domain/dto/UserJoinRequest.java
@@ -2,10 +2,12 @@ package com.hospital.review.domain.dto;
 
 import com.hospital.review.domain.User;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserJoinRequest {
